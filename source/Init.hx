9d5d01e1a9b92cd3674446dcd831c19fca306bb3
Joalor64 <rhgarcon@gmail.com>
2023-06-04 12:44:20 -0400

    Update Init.hx

--- source/Init.hx
+++ source/Init.hx
@@ -11,34 +11,34 @@ using StringTools;
 // since a substate can't be the initial state, we make a temporary state
 class Init extends FlxState 
 {
-	public static var updateVersion:String = '';
     var mustUpdate:Bool = false;
+    public static var updateVersion:String = '';
 
     override function create() 
-	{
+    {
         #if desktop
-		trace('checking for update');
-		var http = new Http("https://raw.githubusercontent.com/Joalor64GH/BandLab-Radio-Player/main/embed/gitVersion.txt");
+	trace('checking for update');
+	var http = new Http("https://raw.githubusercontent.com/Joalor64GH/BandLab-Radio-Player/main/embed/gitVersion.txt");
 
-		http.onData = function(data:String) 
-		{
-			updateVersion = data.split('\n')[0].trim();
-			var curVersion:String = MainMenuState.gameVersion.trim();
-			trace('version online: ' + updateVersion + ', your version: ' + curVersion);
-			if(updateVersion != curVersion) 
-			{
-				trace('versions arent matching!');
-				mustUpdate = true;
-			}
-		}
+	http.onData = function(data:String) 
+	{
+	    updateVersion = data.split('\n')[0].trim();
+	    var curVersion:String = MainMenuState.gameVersion.trim();
+	    trace('version online: ' + updateVersion + ', your version: ' + curVersion);
+	    if(updateVersion != curVersion) 
+	    {
+	        trace('versions arent matching!');
+		mustUpdate = true;
+	    }
+	}
 
-		http.onError = function (error) 
-		{
-			trace('error: $error');
-		}
+	http.onError = function (error) 
+	{
+	    trace('error: $error');
+	}
 
-		http.request();
-		#end
+	http.request();
+	#end
 
         if (mustUpdate) {
             FlxG.switchState(new states.OutdatedState());

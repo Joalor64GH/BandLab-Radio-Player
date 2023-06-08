eaf1a311944dda65324dd84e4efaec4cabadce6f
Joalor64 <rhgarcon@gmail.com>
2023-06-08 11:21:52 -0400

    added fps idk

--- source/Main.hx
+++ source/Main.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxGame;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -13,6 +14,7 @@ class Main extends Sprite
 		base.NativeUtil.enableDarkMode();
 		#end
 
+		addChild(new FPS(10, 3, 0xFFFFFF));
 		addChild(new FlxGame(1280, 720, Init, #if (flixel < "5.0.0") -1, #end 60, 60, false, false));
 	}
 }

2fde260d7c4c679ca6e3fe0b4472088f8f4933f7
Joalor64 <rhgarcon@gmail.com>
2023-07-03 17:08:24 -0400

    removed useless stuff idk

--- source/base/Conductor.hx
+++ source/base/Conductor.hx
@@ -2,23 +2,18 @@ package base;
 
 import flixel.FlxG;
 
-class Conductor
+class Conductor 
 {
 	public static var bpm:Float = 100;
 	public static var crochet:Float = ((60 / bpm) * 1000); // beats in milliseconds
 	public static var stepCrochet:Float = crochet / 4; // steps in milliseconds
 	public static var songPosition:Float;
-	public static var offset:Float = 0;
-
-	public static var safeFrames:Int = 5;
-	public static var safeZoneOffset:Float = (safeFrames / 60) * 1000; // is calculated in create(), is safeFrames in milliseconds
 
 	public function new() {}
 
 	public static function changeBPM(newBpm:Float)
 	{
 		bpm = newBpm;
-
 		crochet = ((60 / bpm) * 1000);
 		stepCrochet = crochet / 4;
 	}
